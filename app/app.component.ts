84fb9cb5dabf9a4130b6f1f89d9b72df865ed10e
zoldello <zoldello@gmail.com>
2016-08-25 20:11:50 -0500

    Displaying List

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,40 @@
 import {  Component } from '@angular/core';
 
+import { Hero } from './hero';
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Dirty Diana' },
+    { id: 12, name: 'Billie Jean' },
+    { id: 13, name: 'Maria' },
+    { id: 14, name: 'Hannah' },
+    { id: 15, name: 'Suzy' },
+    { id: 16, name: 'Susie' },
+    { id: 17, name: 'Elizabeth' }
+];
+
+
 @Component({
     selector: 'my-app',
-    templateUrl: './app/app.component.html'
+    templateUrl: './app/app.component.html',
+    styleUrls: ['./app/app.component.css']
 })
 export class AppComponent {
+    public heroes = HEROES;
+
+
 
 }
+
+
+/*
+Questions
+
+- How to set up service
+- How to add css style via component
+.
+.
+.
+
+- Unit test runner
+- e2e test runner
+*/
